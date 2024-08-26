402c1c91c36753642d39ca83ab9c2163b635072c
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-08-26 22:48:12 +0100

    initial commit

---
+++ src/board.rs
@@ -0,0 +1,3 @@
+// Need to set up all the starting bitboards for all the piece + colour combinations
+// These will be 64 bits
+// TODO: write up all the initial bitboards and render them on a chessboard to see
