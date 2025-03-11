d358b2bafa90e227616c6bec82fe28801d9e82de
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2025-03-11 20:21:42 +0000

    [FEAT] implemented draw Pieces + moves

--- src/main/BoardGraphics.java
+++ src/main/BoardGraphics.java
@@ -1,22 +1,57 @@
 package main;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.List;
 
 class BoardGraphics extends JPanel {
-    Board board = new Board();
-    long[] bitboards = board.getBitboards();
+    static Board board = new Board();
+    static int SQUARE_SIZE = 100;
+    private static List<Integer> possibleMoves= new ArrayList<>();
+
+    public void start(){
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    // need to on the click of a square, extract the square index and find all possible moves where
+    // the starting square is square index
+    public void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("My Chess Engine");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(this);
+
+        frame.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent me) {
+                int clickedSquare = getSquareClicked(me.getX(), me.getY());
+                possibleMoves = getPossibleMoves(clickedSquare);
+                repaint();
+            }
+        });
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
 
 
     public Dimension getPreferredSize() {
-        return new Dimension(800,800);
+        return new Dimension(8 * SQUARE_SIZE,8 * SQUARE_SIZE);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         drawBoard(g);
+        drawPossibleMoves(g, possibleMoves);
     }
 
     private void drawBoard(Graphics g){
@@ -24,50 +59,64 @@ class BoardGraphics extends JPanel {
             for (int column = 0; column < 8; column++) {
                 drawSquare(g, row, column );
 
-                drawPiece(g, row, column);
+                int squareIndex = row * 8 + column;
+                Optional<Piece> pieceOnSquare = board.getPieceFromSquareIndex(squareIndex);
+
+                if (pieceOnSquare.isPresent()){
+                    drawPiece(g, pieceOnSquare.get(), row, column);
+                }
+
             }
         }
     }
 
     private void drawSquare(Graphics g, int row, int column){
-        int SQUARE_SIZE = 100;
-
         // get square colour based on coordinate
-        if ((row + column) % 2 == 1) {
+        if ((row + column) % 2 == 0) {
             g.setColor(new Color(109, 77, 54));
         } else {
             g.setColor(new Color(181, 137, 105));
         }
 
         // draw square
-        g.fillRect(column * SQUARE_SIZE,700 - row * SQUARE_SIZE,SQUARE_SIZE,SQUARE_SIZE);
+        g.fillRect(column * SQUARE_SIZE,7 * SQUARE_SIZE - row * SQUARE_SIZE,SQUARE_SIZE,SQUARE_SIZE);
     }
 
-    private void drawPiece(Graphics g, int row, int column){
-        // given the coordinates of a chess square, check if any of the bitmaps have a 1 there
-        // if so, print the piece out
+    private void drawPiece(Graphics g, Piece piece, int row, int column){
+        g.drawImage(piece.getPieceImage(), column * SQUARE_SIZE,7 * SQUARE_SIZE - row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, this);
+    }
 
-        // will rename this to be smth to check if a piece is present on that square and have another
-        // function that takes in the piece and coordinates and outputs it
-        int squareIndex = row * 8 + column;
+    private static int getSquareClicked(int x, int y){
+        // given (x,y) coordinates of user click
+        // get the index of the square that was clicked
 
-        for (int k = 0; k < bitboards.length; k++) {
-            long bitboard = bitboards[k];
-            if ((bitboard >> squareIndex & 1) != 0) {
-                Piece piece = getPieceFromBitboardIndex(k);
-                g.drawImage(piece.getPieceImage(), column * 100,700 - row * 100, 100, 100, this);
-                // there will never be two pieces on the same square therefore we can stop iterating
-                return;
+        int columnClicked = x / SQUARE_SIZE;
+        int rowClicked = (8 * SQUARE_SIZE -y) / SQUARE_SIZE;
+
+        return rowClicked * 8 + columnClicked;
+    }
+
+    private static List<Integer> getPossibleMoves(int squareIndex){
+        List<Integer> targetSquares = new ArrayList<>();
+        // based on squareIndex, go through allMoves and get all moves which have
+        // squareIndex as startingSquare and return a list of their targetSquares
+        for (Move move : board.getAllMoves()){
+            if (move.getStartingSquare() == squareIndex){
+                targetSquares.add(move.getTargetSquare());
             }
         }
+        return targetSquares;
     }
 
-    private Piece getPieceFromBitboardIndex(int bitboardIndex){
-        Colour pieceColour = (bitboardIndex % 2 == 0) ? Colour.White : Colour.Black;
-        PieceType pieceType = PieceType.values()[bitboardIndex / 2];
-        return new Piece(pieceColour, pieceType);
+    private static void drawPossibleMoves(Graphics g, List<Integer> possibleMoves){
+        int CIRCLE_SIZE = 20;
+        g.setColor(new Color(180, 180, 180, 255));
+        // for each squareIndex in possible moves
+        // draw small circle?
+        for (int possibleMove : possibleMoves){
+            int row = possibleMove / 8;
+            int column = possibleMove % 8;
+            g.fillOval(column * SQUARE_SIZE  + 40, 7 * SQUARE_SIZE - row * SQUARE_SIZE + 40, CIRCLE_SIZE, CIRCLE_SIZE);
+        }
     }
-
-    // need a separate class with the placement of all the pieces, this will get all the piece positions
-    // and display the relevant image at said position
 }
