b57a0df1b18fb2dad156514e99d098c1062d05ee
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2025-03-11 14:14:25 +0000

    [FEAT] draw chessboard

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,25 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("My Chess Engine");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(new BoardGraphics());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
 }
