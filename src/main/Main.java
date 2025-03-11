ad7574350c917a726b2d797f779797dbc12d09e1
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2025-03-11 20:21:13 +0000

    [FEAT] abstracted graphics out of main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,8 @@
 package main;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
-
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("My Chess Engine");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.add(new BoardGraphics());
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
+        BoardGraphics boardGraphics = new BoardGraphics();
+        boardGraphics.start();
     }
 }
