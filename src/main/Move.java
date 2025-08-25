84608efec77d0634548d711e47b02c88ac007c5a
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2025-08-25 13:07:29 +0100

    [FEAT] added captured piece + toString

--- src/main/Move.java
+++ src/main/Move.java
@@ -1,14 +1,19 @@
 package main;
 
+import java.util.Optional;
+
 public class Move {
     private final Piece pieceToMove;
     private final int startingSquare;
     private final int targetSquare;
+    private final Optional<Piece> capturedPiece;
 
-    public Move(Piece pieceToMove, int startingSquare, int targetSquare){
+    public Move(Piece pieceToMove, int startingSquare, int targetSquare, Optional<Piece> capturedPiece){
         this.pieceToMove = pieceToMove;
         this.startingSquare = startingSquare;
         this.targetSquare = targetSquare;
+
+        this.capturedPiece = capturedPiece;
     }
 
     public Piece getPieceToMove() {
@@ -21,5 +26,20 @@ public class Move {
 
     public int getTargetSquare() {
         return targetSquare;
+    }
+
+    public Optional<Piece> getCapturedPiece() {
+        return capturedPiece;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(pieceToMove).append(" from ").append(BoardUtils.getSquareNameFromIndex(startingSquare))
+                .append(" to ").append(BoardUtils.getSquareNameFromIndex(targetSquare));
+
+        capturedPiece.ifPresent(piece -> sb.append(", captures ").append(piece));
+
+        return sb.toString();
     }
 }
